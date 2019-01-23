527e2222ae0f96539321b78ed98c051828d7ac88
pedromassango <pedromassango.developer@gmail.com>
2019-01-23 19:23:37 +0100

    initial commit

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+class SplashPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return SplashPageState();
+  }
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
