417a9a99e15803fa5551ee3744c7b866fe621e84
pedromassango <pedromassango.developer@gmail.com>
2019-01-24 01:13:07 +0100

    splash page done

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
+import 'package:zero/ui/components/page_reveal.dart';
 
 class SplashPage extends StatefulWidget{
   @override
@@ -7,11 +9,85 @@ class SplashPage extends StatefulWidget{
   }
 }
 
-class SplashPageState extends State<SplashPage> {
+class SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+
+  final _navigateAwayDelay = Duration (seconds: 1);
+  final _animationDuration = Duration(seconds: 1);
+  final _startAnimationDelay = Duration(seconds: 3);
+  Animation<double> revealAnimation;
+  AnimationController _animationController;
+
+
+  void _navigateAway() {
+    Navigator.pushNamed(context, '/intro');
+  }
+  
+  @override
+  void initState() {
+    // set app in fullscreen
+    SystemChrome.setEnabledSystemUIOverlays([]);
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: _animationDuration
+    );
+
+    _animationController.addStatusListener((status){
+      if(status == AnimationStatus.completed){
+        Future.delayed(_navigateAwayDelay, (){
+          _navigateAway();
+        });
+      }
+      
+    });
+
+
+    Future.delayed(_startAnimationDelay, (){
+      _animationController.forward();
+    });
+
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    revealAnimation = Tween<double>(
+        begin: 0,
+        end: 100
+    ).animate(
+        CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.linear
+        )
+    );
+
     return Scaffold(
-      body: Container(),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+
+          AnimatedBuilder(
+            animation: revealAnimation,
+            builder: (context, child){
+              return PageReveal(
+                revealPercent: _animationController.value,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  color: Colors.red,
+                ),
+              );
+            },
+          ),
+
+          Center(
+            child: Icon(Icons.adjust, color: Colors.white, size: 90,),
+          )
+        ],
+      ),
     );
   }
+
 }
